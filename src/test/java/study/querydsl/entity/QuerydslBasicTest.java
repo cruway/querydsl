c4de6378ea2887a4f909188413881167a79445a7
cruway <astralkaiser89@gmail.com>
2023-04-02 13:27:57 +0900

    querydsl practice

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl.entity;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
@@ -561,5 +562,30 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_booleanBuilder() throws Exception {
+        String userNameParam = "member1";
+        Integer ageParam = null;
+
+        List<Member> result = searchMember1(userNameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String userNameCond, Integer ageCond) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if(userNameCond != null) {
+            builder.and(member.userName.eq(userNameCond));
+        }
+
+        if(ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
